09feedbf48bf943171500cfde32770c438da7841
Emey Hm <amal.hmidani95@gmail.com>
2024-08-04 00:48:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Zoo import *
+from Lion import *
+from Elephant import *
 
 #create un instanciation zoo
 my_zoo = Zoo()
@@ -8,3 +10,11 @@ lion = Lion("leo","Lion",3)
 elephant = Elephant("ely","Elephant",4)
 moneky = Moneky("moni","Moneky",8)
 
+#add animal to list
+my_zoo.add_animal(lion)
+my_zoo.add_animal(elephant)
+
+#print the animal list
+print("Animals :")
+for animal in my_zoo.list_animals():
+    print(animal)
