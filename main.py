8dc60da88d0ac88744d58590804810bafa8b90dc
Emey Hm <amal.hmidani95@gmail.com>
2024-08-03 22:22:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from Zoo import *
+
+#create un instanciation zoo
+my_zoo = Zoo()
+
+#create animals
+lion = Lion("leo","Lion",3)
+elephant = Elephant("ely","Elephant",4)
+moneky = Moneky("moni","Moneky",8)
+
