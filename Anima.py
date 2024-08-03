8e67856b581a1a956d0868b97d034ae1df8a8a33
Emey Hm <amal.hmidani95@gmail.com>
2024-08-03 21:18:52 +0100

    Update Animal.py
    this an abstract class Animal with abstract method sound( )

--- Anima.py
+++ Anima.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
-class Animal:
+# abstract class we make ABC and we import it from abc
+class Animal(ABC):
 
   #constructeur
   def __init__(self,name,species,age):
@@ -8,7 +9,7 @@ class Animal:
     self.species = species
     self.age = age
     
-  #methode sounds() is abstract so we import it
+  #methode sounds() is abstract so we import abstractmethod
   @abstractmethod
   def make_sound(self):
     pass
