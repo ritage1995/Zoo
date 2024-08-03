6fe6b31dac8299777b7e3c654334f8248d356dd6
Emey Hm <amal.hmidani95@gmail.com>
2024-08-03 22:16:45 +0100

    Update Zoo.py

--- Zoo.py
+++ Zoo.py
@@ -8,6 +8,15 @@ class Zoo:
   def add_animal(self,animal):
     self.animals.append(animal)
 
-  #methode add
+  #methode remove
   def remove_animal(self,name):
     self.animals = [animal for animal in self.animals if animal.name != name]
+
+  #method list
+  def list_animals(self):
+    return [f"{animal.name} the {animal.species}" for animal in self.animals]
+
+  #method make sounds
+  def make_all_sounds(self):
+    return [animal.make_sound() for animal in self.animals]
+  
