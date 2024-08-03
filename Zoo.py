757343544e5b1b2777cd299c03688727686aa0a9
Emey Hm <amal.hmidani95@gmail.com>
2024-08-03 21:44:04 +0100

    Update Zoo.py

--- Zoo.py
+++ Zoo.py
@@ -1,2 +1,13 @@
 class Zoo:
-  pass
+  
+  #constructeur
+  def __init__(self):
+    self.animals = []
+
+  #methode add
+  def add_animal(self,animal):
+    self.animals.append(animal)
+
+  #methode add
+  def remove_animal(self,name):
+    self.animals = [animal for animal in self.animals if animal.name != name]
